cf2f039ee417e8b5d4fa96b8316b2bb2af4449a4
Vladisalv Goteiner <vladislavgoteiner@gmail.com>
2022-08-24 23:56:25 +0300

    another commit

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import {MongoClient, mongodb} from "mongodb";
+
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const url = "mongodb+srv://BigChonk:DFXOzRp8WyH2ixwS@pyosikdb.bdcgazz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url,{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log(req.body);
+    res.send(console.log(req.body));
 })
 
 app.listen(process.env.PORT ||port, () => {
